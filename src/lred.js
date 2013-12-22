f0a5805289c33acb264b292bd2693c1944e08ddb
Vince Au <littlevince09@gmail.com>
2013-12-22 13:47:08 +1100

    Pushed v0.2 release

--- src/lred.js
+++ src/lred.js
@@ -1,9 +1,9 @@
 /*
 	LIKE, REBLOG, EDIT AND DELETE (LRED) BUTTON UNDER EVERY POST
-	LRED (c) 2012 little-vince
+	LRED (c) 2013 little-vince
 	little-vince.tumblr.com
-	Version: 0.1
-	Last edited: 19 October 2012
+	Version: 0.2
+	Last edited: 22 December 2013
 
 	This copyright section and all credits in the script must be included in modifications or redistributions of this script.
 */
@@ -11,24 +11,19 @@
 function little(plink) {
 	// no ReblogURL so we need to find the rk and pid ourselves.
 	// this uses AJAX (slower) but there's nothing we can do about it.
-	var xmlHttp=new XMLHttpRequest();
+	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open("GET",plink,false);
 	xmlHttp.send();
-	var str=xmlHttp.responseText;
-	str=str.slice(str.lastIndexOf("BEGIN TUMBLR CODE"),str.lastIndexOf("END TUMBLR CODE"));
+	var str = xmlHttp.responseText;
+	str = str.slice(str.lastIndexOf("BEGIN TUMBLR CODE"), str.lastIndexOf("END TUMBLR CODE"));
 	// find pid
-	var vvpid=str.substring(str.search("pid=")+4,str.search("pid=")+4+str.substring(str.search("pid=")+4).search("&"));
+	var vvpid = str.substring(str.search("pid=") + 4, str.search("pid=") + 4 + str.substring(str.search("pid=") + 4).search("&"));
 	// find rk
-	var vvrk=str.substring(str.search("rk=")+3,str.search("rk=")+3+str.substring(str.search("rk=")+3).search("&"));
-	return "http://assets.tumblr.com/iframe.html?10&src="+encodeURIComponent(plink)+"&pid="+vvpid+"&rk="+vvrk;
+	var vvrk = str.substring(str.search("rk=") + 3, str.search("rk=") + 3 + str.substring(str.search("rk=") + 3).search("&"));
+	return "&pid=" + vvpid + "&rk=" + vvrk;
 }
+
 function vince(url,plink) {
-	// check if ReblogURL exists (ask posts don't have ReblogURLs!)
-	if (url.indexOf('reblog')!==-1) {
-		// does exist (good!)
-		return "http://assets.tumblr.com/iframe.html?10&src="+encodeURIComponent(plink)+"&pid="+url.split('/')[4]+"&rk="+url.split('/')[5];
-	} else {
-		// doesn't exit (bad...)
-		return little(plink)
-	}
+	var id = (url.indexOf('reblog') !== -1) ? "&pid=" + url.split('/')[4] + "&rk=" + url.split('/')[5] : little(plink);
+	return "http://assets.tumblr.com/assets/html/iframe/o.html?src=" + encodeURIComponent(plink) + id;
 }
