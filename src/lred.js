0435aa5b2b8ab2b2a1d5a5108d5efa834f966784
Vince Au <littlevince09@gmail.com>
2014-01-22 18:00:23 +1100

    Added compatibility with infinite scroll

--- src/lred.js
+++ src/lred.js
@@ -1,13 +1,23 @@
 /*
-	LIKE, REBLOG, EDIT AND DELETE (LRED) BUTTON UNDER EVERY POST
+	LIKE, REBLOG, EDIT AND DELETE (LRED) BUTTON UNDER EVERY TUMBLR POST
 	little-vince.tumblr.com
-	Version: 0.3
-	Last edited: 14 January 2014
+	Version: 0.4
+	Last edited: 22 January 2014
 
 	Copyright (c) 2014 little-vince
 
 	Released under the MIT License, this copyright section and all credits in the script must be included in modifications or redistributions of this script.
 */
+
+var marker = 0;
+
+function addSource(target) {
+	var elements = target.querySelectorAll(".lred");
+	for (; marker < elements.length; marker++) {
+		var e = elements[marker];
+		e.src = vince(e.getAttribute('data-reblog'), e.getAttribute('data-plink'));
+	}
+}
 
 function vince(url, plink) {
 	var id = "&pid=" + url.split('/')[4] + "&rk=" + url.split('/')[5];
@@ -15,9 +25,16 @@ function vince(url, plink) {
 }
 
 window.addEventListener("DOMContentLoaded", function() {
-	var elements = document.getElementsByClassName("lred");
-	for (var i = 0; i < elements.length; i++) {
-		var e = elements[i];
-		e.src = vince(e.getAttribute('data-reblog'), e.getAttribute('data-plink'));
-	}
+	addSource(document.body);
+
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			if (mutation.type === 'childList') {
+				addSource(mutation.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {childList: true, subtree: true});
+
 }, false);
